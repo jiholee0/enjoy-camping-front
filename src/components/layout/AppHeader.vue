0d45960c86213655a21a9530c6e4371ff2d485a6
jiholee0 <dlwlgh1254@naver.com>
2024-11-07 15:57:13 +0900

    feat: 네비게이션 링크를 router-link로 변경하여 SPA 페이지 전환 기능 구현

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,38 +1,48 @@
 <template>
   <header class="header">
-      <div class="logo">
-          <img src="/images/Logo.png" alt="logo" />
-      </div>
-      <nav>
-          <ul>
-              <li><a href="/">HOME</a></li>
-              <li><a href="/viewCampsites">캠핑장 둘러보기</a></li>
-              <li><a href="/viewAttractions">관광지 둘러보기</a></li>
-              <li><a href="/writeReview">리뷰 쓰기</a></li>
-          </ul>
-      </nav>
-      <div class="auth-buttons">
-          <ButtonLight
-              v-if="!isLoggedIn"
-              label="로그인"
-              @click="login"
-          />
-          <ButtonLight
-              v-else
-              label="로그아웃"
-              @click="logout"
-          />
-          <ButtonDark
-              v-if="!isLoggedIn"
-              label="회원가입"
-              @click="signup"
-          />
-          <ButtonDark
-              v-else
-              label="마이페이지"
-              @click="goToMyPage"
-          />
-      </div>
+    <div class="logo">
+      <router-link to="/">
+        <img src="/images/Logo.png" alt="logo" />
+      </router-link>
+    </div>
+    <nav>
+      <ul>
+        <li :class="{ active: isActive('/') }">
+          <router-link to="/">HOME</router-link>
+        </li>
+        <li :class="{ active: isActive('/viewCampsites') }">
+          <router-link to="/viewCampsites">캠핑장 둘러보기</router-link>
+        </li>
+        <li :class="{ active: isActive('/viewAttractions') }">
+          <router-link to="/viewAttractions">관광지 둘러보기</router-link>
+        </li>
+        <li :class="{ active: isActive('/writeReview') }">
+          <router-link to="/writeReview">리뷰 쓰기</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="auth-buttons">
+      <ButtonLight
+        v-if="!isLoggedIn"
+        label="로그인"
+        @click="login"
+      />
+      <ButtonLight
+        v-else
+        label="로그아웃"
+        @click="logout"
+      />
+      <ButtonDark
+        v-if="!isLoggedIn"
+        label="회원가입"
+        @click="signup"
+      />
+      <ButtonDark
+        v-else
+        label="마이페이지"
+        @click="goToMyPage"
+      />
+    </div>
   </header>
 
   <LoginModal :showModal="isLoginModalOpen" @close="isLoginModalOpen = false" />
@@ -47,32 +57,35 @@ import ButtonLight from '../component/ButtonLight.vue';
 
 export default {
   components: {
-      LoginModal,
-      SignupModal,
-      ButtonDark,
-      ButtonLight
+    LoginModal,
+    SignupModal,
+    ButtonDark,
+    ButtonLight
   },
   data() {
-      return {
-          isLoggedIn: false,
-          isLoginModalOpen: false,
-          isSignupModalOpen: false
-      };
+    return {
+      isLoggedIn: false,
+      isLoginModalOpen: false,
+      isSignupModalOpen: false
+    };
   },
   methods: {
-      login() {
-          this.isLoginModalOpen = true;
-      },
-      signup() {
-          this.isSignupModalOpen = true;
-      },
-      logout() {
-          this.isLoggedIn = false;
-      },
-      goToMyPage() {
-          // 마이페이지 이동 로직 추가
-      },
-  },
+    login() {
+      this.isLoginModalOpen = true;
+    },
+    signup() {
+      this.isSignupModalOpen = true;
+    },
+    logout() {
+      this.isLoggedIn = false;
+    },
+    goToMyPage() {
+      // 마이페이지 이동 로직 추가
+    },
+    isActive(path) {
+      return this.$route.path === path;
+    }
+  }
 };
 </script>
 
@@ -110,10 +123,20 @@ nav ul {
   font-size: 0.8rem;
 }
 
+nav ul li {
+  position: relative;
+}
+
 nav ul li a {
   text-decoration: none;
   color: #555;
   white-space: nowrap;
+}
+
+nav ul li.active a {
+  color: #0077b6; /* 활성화된 링크 색상 변경 */
+  font-weight: bold; /* 강조를 위한 볼드체 */
+  border-bottom: 2px solid #0077b6; /* 하단에 강조를 위한 border 추가 */
 }
 
 .auth-buttons {
